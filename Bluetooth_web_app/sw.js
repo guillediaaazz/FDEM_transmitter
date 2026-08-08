8e29ec10e609c97d30c46dbc1d3a85f57c0f0a4f
guillediaaazz <guillediazmata@gmail.com>
2026-08-08 14:30:46 +0200

    Creados archivos necesarios para que la interfaz web pueda funcionar offline

---
+++ Bluetooth_web_app/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'fdem-v1';
+const ASSETS = [
+  './FDEM_transmitter_control.html',
+  './manifest.json'
+];
+
+// Install event: cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch event: serve from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
